f84c5ef8f824895d0ffd1516185604f178613e34
Mariia Mikhailova <maria.mihailova@touchin.ru>
2014-06-03 16:53:28 +0400

    initialisation

---
+++ MainView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MonoTouch.UIKit;
+
+namespace TwitterBot
+{
+	public class MainView: UIView
+	{
+		public MainView ()
+		{
+			BackgroundColor = UIColor.Blue;
+		}
+	}
+}
+
---
+++ MyController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MonoTouch.UIKit;
+
+namespace TwitterBot
+{
+	public class MyController : UIViewController
+	{
+		public MyController ()
+		{
+		}
+
+		public override void LoadView ()
+		{
+			View = new MainView ();
+		}
+	}
+}
+
