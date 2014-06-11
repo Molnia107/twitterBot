8602da4bc4587d998392770c8802c3bcf3254ea5
Mariia Mikhailova <maria.mihailova@touchin.ru>
2014-06-11 16:37:32 +0400

    создан интерфейс для реципиента и наследник RestRequest для обработки асинхронных запросов к твитер

---
+++ Interfaces.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TwitterBot
+{
+	public interface IRecepient
+	{
+		void SetTwitts(List<Twitt> twittList);
+		void SetNetworkError ();
+		void Authontificate (string url);
+		void SetAuthorizationRezult (bool rezult);
+	}
+}
+
---
+++ ShyBotRestRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using RestSharp;
+
+namespace TwitterBot
+{
+	public class ShyBotRestRequest : RestRequest
+	{
+		public IRecepient Recepient;
+
+		public ShyBotRestRequest ():base()
+		{
+		}
+
+		public ShyBotRestRequest(string resource, Method method, IRecepient recepient):base(resource, method)
+		{
+			Recepient = recepient;
+		}
+	}
+}
+
